fd4b7957ce9f853e3ba68a4cfdcf984cabe49aeb
dingxuewu <2497436726@qq.com>
2021-11-03 07:30:35 +0800

    条件渲染

--- src/index.js
+++ src/index.js
@@ -1,17 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const title = React.createElement('h1', null, 'Hello React!',<App/>)
+let isLoading = true;
+
+const loadData = () => {
+  return isLoading ? (<div>loading...</div>) : (<div>数据加载完成~</div>)
+}
+const title = (<h2>请求数据中{loadData()}</h2>)
 ReactDOM.render(title, document.getElementById('root'))
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>,
-//   document.getElementById('root')
-// );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
