7c0eb93685753fce22201749626f1e0dcca1c9ec
dingxuewu <2497436726@qq.com>
2021-11-04 07:08:29 +0800

    抽离为单独JS文件

---
+++ src/components/Hello.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+// 创建组件
+const Hello = () => (
+  <h2>我是Hello组件！</h2>
+)
+
+// 导出组件
+export default Hello;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const title = React.createElement('h1', null, 'Hello React!',<App/>)
-ReactDOM.render(title, document.getElementById('root'))
+// 导入组件
+import Hello from './components/Hello'
 
-reportWebVitals();
+// 渲染组件
+ReactDOM.render(<Hello />, document.getElementById('root'))
+
+
