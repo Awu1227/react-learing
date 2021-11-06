4887a7e91dc12f4ac83c211a701f006dfab8715e
dingxuewu <2497436726@qq.com>
2021-11-06 10:46:24 +0800

    受控组件

--- src/index.js
+++ src/index.js
@@ -1,10 +1,64 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const title = React.createElement('h1', null, 'Hello React!',<App/>)
-ReactDOM.render(title, document.getElementById('root'))
 
-reportWebVitals();
+class Home extends React.Component {
+  state = {
+    txt: '',
+    content: '',
+    city: 'bj',
+    isChecked: true,
+  }
+  handleChange = e => {
+    this.setState({
+      txt: e.target.value,
+    })
+  }
+  handleContent = e => {
+    this.setState({
+      content: e.target.value,
+    })
+  }
+  handleCity = e => {
+    this.setState({
+      city: e.target.value,
+    })
+  }
+  handleChecked = e => {
+    this.setState({
+      isChecked: e.target.checked
+    })
+  }
+  render() {
+    return (
+      <div>
+        {/*文本框*/}
+        <input type="text" value={this.state.txt} onChange={this.handleChange}></input><br/>
+        <em>文本框的内容：{this.state.txt}</em>
+        <hr/>
+        {/*富文本框*/}
+        <textarea value={this.state.content} onChange={this.handleContent}></textarea><br />
+        <em>富文本框的内容：{this.state.content}</em>
+        <hr />
+        {/*下拉框*/}
+        <select value={this.state.city} onChange={this.handleCity}>
+          <option value='bj'>北京</option>
+          <option value='sh'>上海</option>
+          <option value='gz'>广州</option>
+          <option value='sz'>深圳</option>
+        </select><br />
+        <em>下拉框的内容：{this.state.city}</em>
+        <hr />
+        {/*复选框*/}
+        <label htmlFor="sex">
+        <input type="checkbox" id="sex" checked={this.state.isChecked} onChange={this.handleChecked}/>男
+        </label><br />
+        <em>复选框是否选中：{ console.log(this.state.isChecked) }</em>
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(<Home />, document.getElementById('root'))
+
+
