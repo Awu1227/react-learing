ad4737956b419c2779bb432da027cee39de67d2f
dingxuewu <2497436726@qq.com>
2021-11-07 18:10:23 +0800

    组件的props

--- src/index.js
+++ src/index.js
@@ -1,91 +1,39 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import "./index.css"
-class App extends React.Component {
-  // 初始化状态
-  state = {
-    clocks: [
-      { id: 1, time: '6：00', content: '起床啦！！！'},
-      { id: 2, time: '7：00', content: '吃饭啦~'},
-      { id: 3, time: '8：00', content: '学习啦~'},
-    ],
-    // 闹钟时间
-    usertime : '',
-    // 提示内容
-    userContent : ''
-  }
-
-  // 处理文本框事件
-  handleForm = e => {
-    const {value, name} = e.target
-    this.setState({
-      [name]: value
-    })
-  }
-  // 是否渲染列表
-  renderList() {
-    return this.state.clocks.length ? (
-      <ul>
-      {this.state.clocks.map(item => (
-        <li key={item.id}>
-          <div>
-          <h3>时间：{item.time}</h3>
-          <p>提示内容：{item.content}</p>
-          </div>
-          <button id={item.id} className="deleteBtn" onClick={this.deleteClock}></button>
-          </li>
-      ))}
-    </ul>
-    ) : (
-      <div>没有闹钟，快去设置吧~</div>
-    )
-  }
-  // 增加闹钟
-  addClock = () => {
-    let { clocks, usertime, userContent } = this.state
-    if (!usertime || !userContent) {
-      alert("请输入闹钟时间或闹钟信息!")
-    } else {
-      // 将评论信息添加到state中
-      const newClocks = [{
-        id: Math.random(),
-        time: usertime,
-        content: userContent,
-      }, ...clocks]
-      this.setState({
-        clocks: newClocks,
-        usertime: '',
-        userContent: ''
-      })
-
-    }
-  }
-  // 删除闹钟
-  deleteClock = e => {
-    const id = Number(e.target.id)
-    const {clocks} = this.state
-    const newClocks = clocks.filter(item => item.id !== id)
-    this.setState({
-      clocks: newClocks
-    })
+/**
+ * props
+ */
+const App1 = (props) => {
+  console.log(props);
+  return (
+    <div>
+      <p>函数组件props：{props.name }</p>
+    </div>
+  )
+}
+class App2 extends React.Component {
+  // 推荐使用props作为constructor的参数
+  constructor(props) {
+    super(props)
+    console.log(props);
   }
   render() {
-    const {usertime, userContent} = this.state
     return (
-      <div className="app">
-        <div>
-        <input className="user" type="text" placeholder="请输入闹钟时间" value={usertime} name="usertime" onChange={this.handleForm}/>
-        <textarea className="user-comment" placeholder="请输入提示内容" value={userContent} name="userContent" onChange={this.handleForm}></textarea>
-          <button className="btn-setting"onClick={this.addClock}>设置闹钟</button>
-        </div>
-        {/* 通过条件渲染决定渲染什么内容 */}
-        {this.renderList()}
+      <div>
+        <p>类组件props:{this.props.name} { this.props.age }</p>
       </div>
     )
   }
 }
-
-ReactDOM.render(<App />, document.getElementById('root'))
+/**
+ * 传递数据
+ */
+ReactDOM.render(<App2
+  name="橙子"
+  age={18}
+  colors={['red', 'green', 'yellow']}
+  fn={() => console.log('这是一个函数')}
+  tag={<p>这是一个p标签</p>} />, document.getElementById('root'))
 
 
